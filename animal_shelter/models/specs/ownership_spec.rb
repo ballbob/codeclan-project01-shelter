d0026e990509d433135012082f5b2fb1b287da7f
Robert Ball <robert.ball.1989@gmail.com>
2017-02-24 10:21:00 +0000

    Ownership class tested and accessors working.

--- animal_shelter/models/specs/ownership_spec.rb
+++ animal_shelter/models/specs/ownership_spec.rb
@@ -5,11 +5,15 @@ require_relative('../ownership.rb')
 class TestOwnership < Minitest::Test
 
   def setup()
-    @ownership1 = ({"owner_id" => "1", "animal_id" => "2"})
+    @ownership1 = Ownership.new({"owner_id" => "1", "animal_id" => "2"})
   end
 
   def test_owner_id
     assert_equal(@ownership1.owner_id, "1")
+  end
+
+  def test_animal_id
+    assert_equal(@ownership1.animal_id, "2")
   end
 
 end
