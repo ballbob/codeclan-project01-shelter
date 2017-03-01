6ef11bc24676a571bb4da71e853424d584a4c356
Robert Ball <robert.ball.1989@gmail.com>
2017-03-01 22:35:50 +0000

    More changes to try to get update function for ownership to pass the correct ownership in.

--- animal_shelter/controllers/ownership_controller.rb
+++ animal_shelter/controllers/ownership_controller.rb
@@ -45,8 +45,6 @@ get '/shelter/ownerships/:id/update' do
 end
 
 post '/shelter/ownerships/:id/update' do
-  @animals = Animal.all
-  @owners = Owner.all
   @ownership = Ownership.new(params)
   @ownership.update
   redirect to ('/shelter/ownerships')
